e78935b0c4ddc6f158486e7c05d83fcd2cb9069b
Jungea <jungea2241@gmail.com>
2023-12-17 16:48:38 +0900

    [블로그 웹 앱] 웹뷰 기능 개발

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -1,13 +1,58 @@
 import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
 
 class HomeScreen extends StatelessWidget {
+  WebViewController? controller;
 
-  const HomeScreen({Key? key}) : super(key: key);
+  HomeScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Text('Home Screen'),
+      appBar: AppBar(
+        backgroundColor: Color(0xff042B59),
+        title: Text('Flutter'),
+        centerTitle: true,
+        actions: [
+          IconButton(
+            onPressed: () {
+              if (controller != null) {
+                controller!.goBack();
+              }
+            },
+            icon: Icon(
+              Icons.navigate_before,
+            ),
+          ),
+          IconButton(
+            onPressed: () {
+              if (controller != null) {
+                controller!.goForward();
+              }
+            },
+            icon: Icon(
+              Icons.navigate_next,
+            ),
+          ),
+          IconButton(
+            onPressed: () {
+              if (controller != null) {
+                controller!.loadUrl('https://flutter.dev/');
+              }
+            },
+            icon: Icon(
+              Icons.home,
+            ),
+          )
+        ],
+      ),
+      body: WebView(
+        onWebViewCreated: (WebViewController controller) {
+          this.controller = controller;
+        },
+        initialUrl: 'https://flutter.dev/',
+        javascriptMode: JavascriptMode.unrestricted,
+      )
     );
   }
 
