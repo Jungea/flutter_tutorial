58e804aaa5c9c5c309f8ec90fb6115393da6d65b
Jungea <jungea2241@gmail.com>
2023-12-17 13:45:18 +0900

    제스처 관련 위젯

--- lib/main.dart
+++ lib/main.dart
@@ -10,14 +10,60 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         body: Center(
-          child: Text(
-            'Hello Flutter~!',
-            style: TextStyle(
-              fontSize: 16.0,
-              fontWeight: FontWeight.w700,
-              color: Colors.blue,
-            ),
-          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              TextButton(
+                onPressed: () {},
+                style: TextButton.styleFrom(
+                  foregroundColor: Colors.red,
+                ),
+                child: Text('텍스트 버튼'),
+              ),
+              OutlinedButton(
+                  onPressed: () {},
+                  style: OutlinedButton.styleFrom(
+                    foregroundColor: Colors.red,
+                  ),
+                  child: Text('아웃라인드 버튼')
+              ),
+              ElevatedButton(
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.red,
+                  ),
+                  child: Text('엘레베이티드 버튼'),
+              ),
+              IconButton(
+                  onPressed: () {},
+                  icon: Icon(
+                    Icons.home,
+                  ),
+              ),
+              GestureDetector(
+                onTap: () {
+                  print('on tap');
+                },
+                onDoubleTap: () {
+                  print('on double tap');
+                },
+                onLongPress: () {
+                  print('on long press');
+                },
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Colors.red,
+                  ),
+                  width: 100.0,
+                  height: 100.0,
+                ),
+              ),
+              FloatingActionButton(
+                onPressed: () {},
+                child: Text('클릭'),
+              ),
+            ],
+          )
         ),
       ),
     );
