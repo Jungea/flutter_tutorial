257eb4ad42af0382f8b030a9e0f331c36df4bd8d
Jungea <jungea2241@gmail.com>
2023-12-17 15:31:18 +0900

    main.dart 초기화

--- lib/main.dart
+++ lib/main.dart
@@ -2,40 +2,6 @@ import 'package:flutter/material.dart';
 
 void main() {
   // runApp(MyApp());
-  runApp(StackWidgetExample());
-}
-
-class StackWidgetExample extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        body: SizedBox(
-
-          width: double.infinity,
-          child: Stack(
-            children: [
-              Container(
-                height: 300.0,
-                width: 300.0,
-                color: Colors.red,
-              ),
-              Container(
-                height: 250.0,
-                width: 250.0,
-                color: Colors.yellow,
-              ),
-              Container(
-                height: 200.0,
-                width: 200.0,
-                color: Colors.blue,
-              ),
-            ],
-          ),
-        ),
-      ),
-    );
-  }
 }
 
 class MyApp extends StatelessWidget {
