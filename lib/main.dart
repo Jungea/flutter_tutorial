b5288a0538619912d40f6e3a2f1d3f76267da8b8
Jungea <jungea2241@gmail.com>
2023-12-17 14:07:46 +0900

    디자인 관련 위젯 - Container

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,7 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  // runApp(MyApp());
-  runApp(FloatingActionButtonExample());
-}
-
-class FloatingActionButtonExample extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {},
-          child: Text('클릭'),
-        ),
-      ),
-    );
-  }
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -25,60 +10,18 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              TextButton(
-                onPressed: () {},
-                style: TextButton.styleFrom(
-                  foregroundColor: Colors.red,
-                ),
-                child: Text('텍스트 버튼'),
+          child: Container(
+            decoration: BoxDecoration(
+              color: Colors.red,
+              border: Border.all(
+                width: 16.0,
+                color: Colors.black,
               ),
-              OutlinedButton(
-                  onPressed: () {},
-                  style: OutlinedButton.styleFrom(
-                    foregroundColor: Colors.red,
-                  ),
-                  child: Text('아웃라인드 버튼')
+              borderRadius: BorderRadius.circular(
+                  16.0,
               ),
-              ElevatedButton(
-                  onPressed: () {},
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.red,
-                  ),
-                  child: Text('엘레베이티드 버튼'),
-              ),
-              IconButton(
-                  onPressed: () {},
-                  icon: Icon(
-                    Icons.home,
-                  ),
-              ),
-              GestureDetector(
-                onTap: () {
-                  print('on tap');
-                },
-                onDoubleTap: () {
-                  print('on double tap');
-                },
-                onLongPress: () {
-                  print('on long press');
-                },
-                child: Container(
-                  decoration: BoxDecoration(
-                    color: Colors.red,
-                  ),
-                  width: 100.0,
-                  height: 100.0,
-                ),
-              ),
-              FloatingActionButton(
-                onPressed: () {},
-                child: Text('클릭'),
-              ),
-            ],
-          )
+            ),
+          ),
         ),
       ),
     );
