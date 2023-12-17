5cfe58b452d54765a563f0accec9234ad8491fbb
Jungea <jungea2241@gmail.com>
2023-12-17 15:04:03 +0900

    디자인 관련 위젯 - Flexible/Expanded

--- lib/main.dart
+++ lib/main.dart
@@ -2,50 +2,11 @@ import 'package:flutter/material.dart';
 
 void main() {
   // runApp(MyApp());
-  // runApp(RowWidgetExample());
-  runApp(ColumnWidgetExample());
+  // runApp(FlexibleWidgetExample());
+  runApp(ExpeandedWidgetExample());
 }
 
-class RowWidgetExample extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        body: SizedBox(
-
-          height: double.infinity,
-          child: Row(
-
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.center,
-
-            children: [
-              Container(
-                height: 50.0,
-                width: 50.0,
-                color: Colors.red,
-              ),
-              const SizedBox(width: 12.0),
-              Container(
-                height: 50.0,
-                width: 50.0,
-                color: Colors.green,
-              ),
-              const SizedBox(width: 12.0),
-              Container(
-                height: 50.0,
-                width: 50.0,
-                color: Colors.blue,
-              )
-            ],
-          ),
-        ),
-      ),
-    );
-  }
-}
-
-class ColumnWidgetExample extends StatelessWidget {
+class FlexibleWidgetExample extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -54,28 +15,47 @@ class ColumnWidgetExample extends StatelessWidget {
 
           width: double.infinity,
           child: Column(
-
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.center,
-
             children: [
-              Container(
-                height: 50.0,
-                width: 50.0,
-                color: Colors.red,
+              Flexible(
+                flex: 3,
+                child: Container(
+                  color: Colors.blue,
+                ),
               ),
-              const SizedBox(width: 12.0),
-              Container(
-                height: 50.0,
-                width: 50.0,
-                color: Colors.green,
+              Flexible(
+                flex: 1,
+                child: Container(
+                  color: Colors.red,
+                ),
               ),
-              const SizedBox(width: 12.0),
-              Container(
-                height: 50.0,
-                width: 50.0,
-                color: Colors.blue,
-              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class ExpeandedWidgetExample extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: SizedBox(
+
+          width: double.infinity,
+          child: Column(
+            children: [
+              Expanded(
+                child: Container(
+                  color: Colors.blue,
+                ),
+              ),
+              Expanded(
+                child: Container(
+                  color: Colors.red,
+                ),
+              ),
             ],
           ),
         ),
