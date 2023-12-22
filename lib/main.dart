f4e34a9beb4cb289ac709fd8f183c738a3af9a69
Jungea <jungea2241@gmail.com>
2023-12-23 01:10:54 +0900

    [U&I] CupertinoDatePicker

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,32 @@
-import 'package:flutter_tutorial/screen/image_carousel.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_tutorial/screen/u_and_i.dart';
 
 void main() {
   runApp(MaterialApp(
-    home: ImageCarousel(),
+    theme: ThemeData(
+      fontFamily: 'sunflower',
+      textTheme: TextTheme(
+        displayLarge: TextStyle(
+          color: Colors.white,
+          fontSize: 80.0,
+          fontWeight: FontWeight.w700,
+          fontFamily: 'parisienne',
+        ),
+        displayMedium: TextStyle(
+          color: Colors.white,
+          fontSize: 50.0,
+          fontWeight: FontWeight.w700,
+        ),
+        bodyLarge: TextStyle(
+          color: Colors.white,
+          fontSize: 30.0,
+        ),
+        bodyMedium: TextStyle(
+          color: Colors.white,
+          fontSize: 20.0,
+        ),
+      )
+    ),
+    home: UAndI(),
   ));
 }
